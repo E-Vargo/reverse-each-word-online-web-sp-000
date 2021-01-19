5fb46f625abbe5ed0df47923d5cd9511bd29287a
Elliott Vargo <vargoelliott@gmail.com>
2021-01-19 04:03:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 string = "Hello there, and how are you?"
 def reverse_each_word(string)
 array = string.split(" ")
-array1 = array.each {|i| i.reverse}
+array1 = array.each {|element| element.reverse}
 array1.join(" ")
 end
 
